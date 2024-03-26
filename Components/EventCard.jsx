28735f9093f4d961667866a8e207e706a4bf3d02
mhtpsd <themhtpsd@gmail.com>
2024-03-26 19:32:02 +0530

    Added comments and formated code

--- Components/EventCard.jsx
+++ Components/EventCard.jsx
@@ -1,21 +1,50 @@
+//Card functiom
+
 function EventCard({ event, onInputChange, onDelete, disabled }) {
+  return (
+    <div className="flex flex-col w-fit h-[200px] items-center justify-between text-xl my-5 p-2">
+      <input
+        className="w-fit text-center"
+        type="text"
+        value={event.eventName}
+        onChange={(e) => onInputChange("eventName", e.target.value)}
+        disabled={disabled}
+      />
 
-    return (
-      <div className="flex flex-col w-fit h-[200px] items-center justify-between text-xl my-5 p-2">
-        
-          <input className="w-fit text-center" type="text" value={event.eventName} onChange={(e) => onInputChange('eventName', e.target.value)} disabled={disabled}  />
-        
-        <label className="flex justify-between w-full">
-          Amount:
-          <input className="border-none" type="number" value={event.amount} onChange={(e) => onInputChange('amount', e.target.value)} disabled={disabled} />
-        </label>
-        <label className="flex justify-between w-full">
-          Number of Heads:
-          <input className="border-none ml-2" type="number" value={event.numberOfFriends} onChange={(e) => onInputChange('numberOfFriends', e.target.value)} disabled={disabled} />
-        </label>
-        <button className="bg-slate-50 w-[100px] h-[30px] rounded hover:bg-red-500 text-sm" onClick={onDelete} disabled={disabled}>Delete</button>
-      </div>
-    );
-  }
+      <label className="flex justify-between w-full">
+        Amount:
+        <input
+          className="border-none"
+          type="number"
+          value={event.amount}
+          onChange={(e) => onInputChange("amount", e.target.value)}
+          disabled={disabled}
+        />
+      </label>
+      <label className="flex justify-between w-full">
+        Number of Heads:
+        <input
+          className="border-none ml-2"
+          type="number"
+          value={event.numberOfFriends}
+          onChange={(e) => onInputChange("numberOfFriends", e.target.value)}
+          disabled={disabled}
+        />
+      </label>
 
-  export default EventCard;
+      {/* 
+        Button for deleting current card
+        Each card has it's own delete button
+          */}
+      <button
+        className="bg-slate-50 w-[100px] h-[30px] rounded hover:bg-red-500 text-sm"
+        onClick={onDelete}
+        disabled={disabled}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default EventCard;
--- app/page.js
+++ app/page.js
@@ -1,38 +1,50 @@
-"use client"
+"use client"; //Marked use client because it's a client side component
 
-import { useState } from 'react';
+import { useState } from "react"; //for state management
 
-import EventCard from '@/Components/EventCard';
+import EventCard from "@/Components/EventCard";
 
 export default function Home() {
+  const [events, setEvents] = useState([
+    { eventName: "Event 1", amount: 0, numberOfFriends: 0 },
+  ]); //checking state of cards
+  const [splitAmounts, setSplitAmounts] = useState([]); //setting value of splitted amount each time calculate or reset is clicked
+  const [isCalculated, setIsCalculated] = useState(false); //for disabling those buttons which is no need to access after calculate is clicked
 
-  const [events, setEvents] = useState([{ eventName: 'Event 1', amount: 0, numberOfFriends: 0 }]);
-  const [splitAmounts, setSplitAmounts] = useState([]);
-  const [isCalculated, setIsCalculated] = useState(false);
-
+  //Adding new cards
   const addEvent = () => {
-    const newEvent = { eventName: `Event ${events.length + 1}`, amount: 0, numberOfFriends: 0 };
+    const newEvent = {
+      eventName: `Event ${events.length + 1}`,
+      amount: 0,
+      numberOfFriends: 0,
+    };
     setEvents([...events, newEvent]);
   };
 
+  //Deleting cards one at a time
   const deleteEvent = (index) => {
     const newEvents = [...events];
     newEvents.splice(index, 1);
     setEvents(newEvents);
   };
 
+  //Splitting Bill Amount
   const calculateSplit = () => {
-    const newSplitAmounts = events.map(event => event.amount / event.numberOfFriends);
+    const newSplitAmounts = events.map(
+      (event) => event.amount / event.numberOfFriends
+    );
     setSplitAmounts(newSplitAmounts);
     setIsCalculated(true);
   };
 
+  //Resetting states
   const reset = () => {
-    setEvents([{ eventName: 'Event 1', amount: 0, numberOfFriends: 0 }]);
+    setEvents([{ eventName: "Event 1", amount: 0, numberOfFriends: 0 }]);
     setSplitAmounts([]);
     setIsCalculated(false);
   };
 
+  //Taking input from users
   const handleInputChange = (index, field, value) => {
     const newEvents = [...events];
     newEvents[index][field] = value;
@@ -42,26 +54,82 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center my-10">
       <div className="w-full h-12 flex items-center justify-center">
-        <h1 className='text-2xl cursor-default' onClick={reset}>Bill Split Application</h1>
+        <h1 className="text-2xl cursor-default" onClick={reset}>
+          Bill Split Application
+        </h1>
       </div>
-      <hr className='w-full'/>
-      <div className='w-fit'>
-      {events.map((event, index) => (<>
-        <EventCard key={index} event={event} onInputChange={(field, value) => handleInputChange(index, field, value)} onDelete={() => deleteEvent(index)} disabled={isCalculated} />
-        <hr/>
-        </>
-      ))}
+      <hr className="w-full" />
 
+      {/* Rendering Cards
+          One is rendered bydefault for user
+      */}
+      <div className="w-fit">
+        {events.map((event, index) => (
+          <>
+            <EventCard
+              key={index}
+              event={event}
+              onInputChange={(field, value) =>
+                handleInputChange(index, field, value)
+              }
+              onDelete={() => deleteEvent(index)}
+              disabled={isCalculated}
+            />
+            <hr />
+          </>
+        ))}
       </div>
-      <button className='bg-slate-50 mt-4 w-[200px] h-[30px] rounded hover:bg-green-400' onClick={addEvent}>Add Event</button>
-      <button className='bg-slate-50 mt-4 w-[300px] h-[40px] rounded hover:bg-black hover:text-white relative' onClick={calculateSplit}>Calculate</button>
-          <div className='w-[500px] p-5 flex flex-col items-center justify-between'>
-              <hr className='w-[500px]'/>
-            {splitAmounts.map((amount, index) => (
-                <p className='text-2xl' key={index}>{events[index].eventName}: Per head: {amount}</p>
-            ))}
+      <button
+        className="bg-slate-50 mt-4 w-[200px] h-[30px] rounded hover:bg-green-400"
+        onClick={addEvent}
+      >
+        Add Event
+      </button>
+      <button
+        className="bg-slate-50 mt-4 w-[300px] h-[40px] rounded hover:bg-black hover:text-white relative"
+        onClick={calculateSplit}
+      >
+        Calculate
+      </button>
+
+      {/*
+          Showing Splitted Bill Amount
+      */}
+      {isCalculated && (
+        <span className="text-xl font-bold relative my-5">
+          Splitted Bill Amount
+        </span>
+      )}
+      {isCalculated && <hr className="w-[500px]" />}
+
+      <div className="w-[500px] p-5 flex flex-col items-center justify-evenly">
+        {splitAmounts.map((amount, index) => (
+          <div
+            key={index}
+            className="w-full relative flex flex-col items-center"
+          >
+            <div className="w-full flex items-center justify-evenly">
+              <span className="text-lg text-slate-500 italic	">
+                {events[index].eventName}: Per head:{" "}
+              </span>
+              <span className="text-lg ">{Number(amount).toFixed(2)}</span>
+            </div>
           </div>
-          {isCalculated && <button className='bg-slate-50 mt-4 w-[200px] h-[30px] rounded hover:bg-blue-500' onClick={reset}>New</button>}
+        ))}
+        {isCalculated && <hr className="w-[500px]" />}
+      </div>
+
+      {/*
+        Resetting States
+        */}
+      {isCalculated && (
+        <button
+          className="bg-slate-50 mt-4 w-[200px] h-[30px] rounded hover:bg-blue-500"
+          onClick={reset}
+        >
+          New
+        </button>
+      )}
     </div>
   );
 }
